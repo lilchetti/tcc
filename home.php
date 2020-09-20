4755914e20bebf34e492fc33c080c293237d4ded
Giovanna Ap <50549370+giih02@users.noreply.github.com>
2020-09-20 20:59:29 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang=”pt-br”>
+<html>
+
+    <head> 
+        <title>  Tela inicial </title>
+        <meta charset= "utf-8" >
+        <link rel= "stylesheet" href="css.css" type="text/css"> 
+        <!-- LINK PARA PHP\JS <link rel="stylesheet" type="text/css" media="screen" href="main.css"> -->
+        <!-- Adicionando Javascript --></head>
+        <header>   <!-- ADD CODIGO DO CABEÇALHO --> </header>
+
+        <body>
+
+          
+     <h1> Bem vindo... </h1> 
+
+
+               
+    <table>
+        <tr>
+        <td><a href="agendamento.html"> Agendamentos </a></td>
+        <td><a href="prontuario.html"> Prontuário </a></td>
+        <td><a href="exames.html>"> Exames </a> </td>
+        </tr>
+    </table>
+
+       <form method="POST"enctype="multipart/form-data">
+             <h3> Dados para triagem </h3>
+
+          <h1> Colocar carrosel com imagens de propagandas de campanhas </h1>
+
+   </body>
+   <footer>
+      <!-- ADD CODIGO DO RODAPE -->
+   </footer>
+</html>
